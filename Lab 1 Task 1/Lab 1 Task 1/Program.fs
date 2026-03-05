8baf9a87388f4f1ac1b290a9c83e5ef4a3e8ad07
Kriperus <krasnyhaleksandr123@gmail.com>
2026-03-05 23:43:04 +0500

    Add files via upload

---
+++ Lab 1 Task 1/Lab 1 Task 1/Program.fs
@@ -0,0 +1,19 @@
+﻿open System //Стандартные библиотеки
+open Microsoft.FSharp.Core
+
+let result list = 
+    list |> List.map(fun x -> x + 1)
+
+[<EntryPoint>]  // Заздание "Основной функции"
+let main args =
+    printf "Введите размер списка: "    // Интерфейс для пользователя
+    let size = int(Console.ReadLine())  // Ввод количества элементов
+    let list = [    // Ввод элементов в список со значением +1 к вводимому
+        for i in 1..size do
+            printf "Введите элемент списка: "
+            let value = int(Console.ReadLine())
+            yield value
+    ]
+
+    printfn "Ваш список: %A" (result list) // Вывод ответа
+    0
