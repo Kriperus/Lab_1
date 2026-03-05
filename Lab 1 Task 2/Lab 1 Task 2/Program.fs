669bc11b74957811da7b07fd7c0907e810154ce1
Kriperus <krasnyhaleksandr123@gmail.com>
2026-03-05 23:44:32 +0500

    Add files via upload

---
+++ Lab 1 Task 2/Lab 1 Task 2/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open Microsoft.FSharp.Core
+
+let rec summ x : int =  // Функция вычисления произведения нечетных цифр
+     if x % 2 = 0   // Проверка на четность цифры числа
+     then if x = 0  // Вычисление произведения
+          then 1
+          else summ(x/10)  // Рекурсивный уход
+     else x % 10 * summ(x/10)
+     
+[<EntryPoint>]  // Основная функция
+let main args =
+    printf "Введите число: "    // Запрос исходного числа
+    let x = int(Console.ReadLine()) // Ввод исходного числа
+    if (summ x) = 1 // Если все цифры числа четные
+    then printf "Произведение нечетных цифр числа %i: 0" x  
+    else printf "Произведение нечетных цифр числа %i: %i" x (summ x)
+    0
