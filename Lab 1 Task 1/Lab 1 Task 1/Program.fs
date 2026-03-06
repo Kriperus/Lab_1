ba18aacbb4b3926996f7e40299a152c4ea36412d
Kriperus <krasnyhaleksandr123@gmail.com>
2026-03-06 09:26:28 +0500

    Update Program.fs

--- Lab 1 Task 1/Lab 1 Task 1/Program.fs
+++ Lab 1 Task 1/Lab 1 Task 1/Program.fs
@@ -1,19 +1,23 @@
-﻿open System //Стандартные библиотеки
+open System
 open Microsoft.FSharp.Core
 
-let result list = 
-    list |> List.map(fun x -> x + 1)
-
-[<EntryPoint>]  // Заздание "Основной функции"
-let main args =
-    printf "Введите размер списка: "    // Интерфейс для пользователя
-    let size = int(Console.ReadLine())  // Ввод количества элементов
-    let list = [    // Ввод элементов в список со значением +1 к вводимому
+let сreateList size =
+    let list = [    // список со значениями +1
         for i in 1..size do
             printf "Введите элемент списка: "
             let value = int(Console.ReadLine())
-            yield value
+            yield value + 1
     ]
+    
+    list
+    
+[<EntryPoint>]
+let main args =
 
-    printfn "Ваш список: %A" (result list) // Вывод ответа
+    printf "Введите размер списка: "
+    
+    let size = int(Console.ReadLine())
+    let list = сreateList size
+
+    printfn "Искомый список: %A" list
     0
