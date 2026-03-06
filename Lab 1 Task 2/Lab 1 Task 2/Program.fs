2a883e719c66ec935da2c855da9d79b4d196ad33
Kriperus <krasnyhaleksandr123@gmail.com>
2026-03-06 09:22:15 +0500

    Update Program.fs

--- Lab 1 Task 2/Lab 1 Task 2/Program.fs
+++ Lab 1 Task 2/Lab 1 Task 2/Program.fs
@@ -2,19 +2,23 @@ open System
 open Microsoft.FSharp.Core
 
 let rec summ x : int =  // произведение нечетных цифр
-     if x % 2 = 0   // Проверка четности цифры
-     then if x = 0
-          then 1
-          else summ(x/10)
-     else x % 10 * summ(x/10)
+     if x % 2 = 0  then // Проверка четности цифрs
+        if x = 0 then 
+            1
+        else 
+            summ(x/10)
+     else 
+        x % 10 * summ(x/10)
      
 [<EntryPoint>]
 let main args =
+
     printf "Введите число: "
     let x = int(Console.ReadLine())
-    if (summ x) = 1 // Вывести 0 если все цифры четные
-    then printf "Произведение нечетных цифр числа %i: 0" x  
-    else printf "Произведение нечетных цифр числа %i: %i" x (summ x)
+    
+    if (summ x) = 1 then // Вывести 0 если все цифры четные
+        printf "Произведение нечетных цифр числа %i: 0" x  
+    else 
+        printf "Произведение нечетных цифр числа %i: %i" x (summ x)
 
     0
-
